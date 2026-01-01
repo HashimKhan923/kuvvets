27e11c5d941eb1389773289fd0d2d466f35bf30f
HashimKhan923 <khanhash1994@gmail.com>
2026-01-01 19:18:26 +0500

    dwdwewedwdssddwdewefexsxxdczszxwdwdededwdwddhhhhhhdewddwd

--- app/Http/Controllers/Admin/DepartmentController.php
+++ app/Http/Controllers/Admin/DepartmentController.php
@@ -10,7 +10,7 @@ class DepartmentController extends Controller
 {
     public function index(Request $request)
     {
-        $departments = Department::with('department_managers')->get();
+        $departments = Department::all();
 
             return response()->json(['departments'=>$departments]);  
 
@@ -22,22 +22,14 @@ class DepartmentController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'manager_ids' => 'nullable|array',
-            'manager_ids.*' => 'exists:users,id', // Assuming managers are users
+
         ]);
 
         $department = Department::create([
             'name' => $request->name,
         ]);
 
-        if ($request->has('manager_ids')) {
-            foreach ($request->manager_ids as $id) {
-                DepartmentManager::create([
-                    'department_id' => $department->id,
-                    'manager_id' => $id,
-                ]);
-            }
-        }
+
 
         return response()->json([
             'status' => true,
@@ -50,25 +42,13 @@ class DepartmentController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255',
-            'manager_ids' => 'nullable|array',
-            'manager_ids.*' => 'exists:users,id',
+
         ]);
 
         $department = Department::findOrFail($request->department_id);
         $department->name = $request->name;
         $department->save();
 
-        // Sync managers: delete old and insert new
-        DepartmentManager::where('department_id', $department->id)->delete();
-
-        if ($request->has('manager_ids')) {
-            foreach ($request->manager_ids as $managerId) {
-                DepartmentManager::create([
-                    'department_id' => $department->id,
-                    'manager_id' => $managerId,
-                ]);
-            }
-        }
 
         return response()->json([
             'status' => true,
