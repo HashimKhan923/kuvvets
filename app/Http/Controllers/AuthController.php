16b1190550cda63ae8af9766f5ce036f6819b03e
HashimKhan923 <khanhash1994@gmail.com>
2026-01-01 13:04:56 +0500

    dwdwewedwdssddwdew

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
             return response(['errors' => $validator->errors()->all()], 422);
         }
 
-        $user = User::with('personalInfo', 'location', 'shift')
+        $user = User::with('location', 'shift')
             ->where('email', $request->email)
             ->first();
 
